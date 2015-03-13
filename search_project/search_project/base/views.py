beff43c78586a382b99b0282046af8710b722549
andersonberg <andersonberg@gmail.com>
2015-03-13 11:24:11 -0300

    paginação e static files do chosen

--- search_project/search_project/base/views.py
+++ search_project/search_project/base/views.py
@@ -5,7 +5,9 @@ from search_project.base.models import UserBase, Log
 
 class UserBaseList(ListView):
     model = UserBase
+    paginate_by = 500
 
 
 class LogList(ListView):
     model = Log
+    paginate_by = 500
